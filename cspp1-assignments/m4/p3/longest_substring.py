3e14c2160de2ddec1104d4116c514ba8f9bd38e7
navin106 <sandula.navin@msitprogram.net>
2018-08-02 23:36:23 +0530

    " -> 5 of 5 passed. pylint: -11.05/10 "

---
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -0,0 +1,45 @@
+'''Assume s is a string of lower case characters.
+
+Write a program that prints the longest substring of s in which the letters occur in alphabetical order.
+For example, if s = 'azcbobobegghakl', then your program should print
+
+Longest substring in alphabetical order is: beggh
+
+In the case of ties, print the first substring.
+For example, if s = 'abcbcd', then your program should print
+
+Longest substring in alphabetical order is: abc
+
+Note: This problem may be challenging. We encourage you to work smart.
+If you've spent more than a few hours on this problem, we suggest that you move on to a different part of the course.
+If you have time, come back to this problem after you've had a break and cleared your head.
+'''
+
+def main():
+	'''
+	# the input string is in s
+	# remove pass and start your code here
+	'''
+	s = input()
+	r = ''
+	c = ''
+	for char in s:
+		if(c == ''):
+			c = char
+		elif (c[-1] <= char):
+			c = c+char
+		elif (c[-1] > char):
+			if(len(r) < len(c)):
+				r=c
+				c=char
+			else:
+				c=char
+	if (len(c) > len(r)):
+		r=c
+	print(r)
+
+
+
+
+if __name__== "__main__":
+	main()
