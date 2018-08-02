fbbabac8be8786b5833ca953fc25b48da23f7795
navin106 <sandula.navin@msitprogram.net>
2018-08-02 22:29:48 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-assignments/m4/p2/bob_counter.py
+++ cspp1-assignments/m4/p2/bob_counter.py
@@ -7,17 +7,17 @@ Write a program that prints the number of times the string 'bob' occurs in s.
 Number of times bob occurs is: 2
 '''
 def main():
-	'''
-	# the input string is in s
-	# remove pass and start your code here
-	'''
-	s_str = input()
-	c_len = len(s_str)
-	i_lo = 0
-	k_co = 0
-	for i_lo in range(k_co):
-		if s_str[i_lo:i_lo+3] == 'bob':
-			k_co = k_co+1
-	print(k_co)
-if __name__== "__main__":
-	main()
+    '''
+    # the input string is in s
+    # remove pass and start your code here
+    '''
+    s_str = input()
+    c_len = len(s_str)
+    i_lo = 0
+    k_co = 0
+    for i_lo in range(c_len):
+        if s_str[i_lo:i_lo+3] == 'bob':
+            k_co = k_co+1
+    print(k_co)
+if __name__ == "__main__":
+    main()
