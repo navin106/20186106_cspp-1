fd15fddc0abcd5190022aba7e1a56c3c0465dd2b
navin106 <sandula.navin@msitprogram.net>
2018-08-02 23:52:17 +0530

    " -> 5 of 5 passed. pylint: 8.95/10 "

--- cspp1-assignments/m4/p3/longest_substring.py
+++ cspp1-assignments/m4/p3/longest_substring.py
@@ -11,32 +11,33 @@ For example, if s = 'abcbcd', then your program should print
 Longest substring in alphabetical order is: abc
 
 Note: This problem may be challenging. We encourage you to work smart.
-If you've spent more than a few hours on this problem, we suggest that you move on to a different part of the course.
+If you've spent more than a few hours on this problem, 
+we suggest that you move on to a different part of the course.
 If you have time, come back to this problem after you've had a break and cleared your head.
 '''
 
 def main():
-	'''
-	# the input string is in s
-	# remove pass and start your code here
-	'''
-	x_str = input()
-	y_str = ''
-	z_str = ''
-	for char in x_str:
-		if(z_str == ''):
-			z_str = char
-		elif (z_str[-1] <= char):
-			z_str = z_str+char
-		elif (z_str[-1] > char):
-			if(len(y_str) < len(z_str)):
-				y_str=z_str
-				z_str=char
-			else:
-				z_str=char
-	if (len(z_str) > len(y_str)):
-		y_str=z_str
-	print(y_str)
+    '''
+    # the input string is in s
+    # remove pass and start your code here
+    '''
+    x_str = input()
+    y_str = ''
+    z_str = ''
+    for char in x_str:
+        if z_str == '':
+            z_str = char
+        elif z_str[-1] <= char:
+            z_str = z_str+char
+        elif z_str[-1] > char:
+            if len(y_str) < len(z_str):
+                y_str = z_str
+                z_str = char
+            else:
+                z_str = char
+    if len(z_str) > len(y_str):
+        y_str = z_str
+    print(y_str)
 
-if __name__== "__main__":
-	main()
+if __name__ == "__main__":
+    main()
