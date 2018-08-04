0529cc87b45919910d2f71ed52fb9d2c55d6e6a4
navin106 <sandula.navin@msitprogram.net>
2018-08-05 03:59:21 +0530

    " -> 0 of 3 passed. pylint: -14.29/10 "

---
+++ cspp1-assignments/m5/p3/square_root_bisection.py
@@ -0,0 +1,30 @@
+# Write a python program to find the square root of the given number
+# using approximation method
+
+# testcase 1
+# input: 25
+# output: 4.999999999999998
+
+# testcase 2
+# input: 49
+# output: 6.999999999999991
+
+def main():
+	'''
+	# epsilon and step are initialized
+	# don't change these values
+	'''
+	s = input()
+	epsilon = 0.01
+	lower = 0.0
+	high = s
+	mid = (low+high)/2.0
+	while abs(mid**2-num) >= epsilon:
+		if mid**2 < num:
+			low = mid
+		else:
+			high = mid
+		mid = (low+high)/2.0
+	print(mid)	
+if __name__== "__main__":
+	main()
