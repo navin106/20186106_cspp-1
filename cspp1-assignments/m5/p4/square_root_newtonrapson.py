2a6ffc8ead95c28ebad468bf563964cf4538d5ca
navin106 <sandula.navin@msitprogram.net>
2018-08-05 04:38:24 +0530

    " -> 3 of 3 passed. pylint: 7.78/10 "

--- cspp1-assignments/m5/p4/square_root_newtonrapson.py
+++ cspp1-assignments/m5/p4/square_root_newtonrapson.py
@@ -2,26 +2,23 @@
 @author = navin106
 # Write a python program to find the square root of the given number
 # using approximation method
-
 # testcase 1
 # input: 25
 # output: 4.999999999999998
-
 # testcase 2
 # input: 49
 # output: 6.999999999999991
 '''
 def main():
-	'''
-	# epsilon and step are initialized
-	# don't change these values
-	'''
-	epsilon = 0.01
-	s = int(input())
-	guess = s/2.0
-	while abs(guess*guess-s)>=epsilon:
-		guess = guess-(((guess**2)-s)/(2*guess))
-	print(str(guess))
-
+    '''
+    # epsilon and step are initialized
+    # don't change these values
+    '''
+    epsilon = 0.01
+    s = int(input())
+    guess = s/2.0
+    while abs(guess*guess-s)>=epsilon:
+        guess = guess-(((guess**2)-s)/(2*guess))
+    print(str(guess))
 if __name__ == "__main__":
-	main()
+    main()
