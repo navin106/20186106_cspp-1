413f913a7b716cf7f9f8cecc81ed1e4ad8787401
navin106 <sandula.navin@msitprogram.net>
2018-08-04 17:07:34 +0530

    " -> 7 of 7 passed. pylint: -6.11/10 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -12,16 +12,18 @@ int_input = int(input())
 pro =1
 rem =0
 temp_int = int_input
-if temp_int<0:
-	temp_int=abs(temp_int)
-while temp_int>0:
-    rem =temp_int%10
-    pro = pro*rem
-    temp_int = temp_int//10
-if int_input<0:
-	print(-pro)
+if int_input!=0:
+	if temp_int<0:
+		temp_int=abs(temp_int)
+	while temp_int>0:
+	    rem =temp_int%10
+	    pro = pro*rem
+	    temp_int = temp_int//10
+	if int_input<0:
+		print(-pro)
+	else:
+		print (pro)
 else:
-	print (pro)
-
+	print(int_input)
 if __name__ == "__main__":
     main()
