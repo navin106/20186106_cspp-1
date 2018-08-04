635a08e32cfd4f0763be103c3c065b574e4014a0
navin106 <sandula.navin@msitprogram.net>
2018-08-05 03:39:32 +0530

    " -> 3 of 3 passed. pylint: -3.33/10 "

--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -10,22 +10,16 @@
 # output: 6.999999999999991
 
 def main():
- x = 25
-epsilon = 0.01
-numGuesses = 0
-low = 1.0
-high = x
-ans = (high + low)/2.0
-while abs(ans**2 - x) >= epsilon:
-	print('low = ' + str(low) + ' high = ' + str(high) + ' ans = ' + str(ans))
-	numGuesses += 1
-if ans**2 < x:
-	low = ans
-else:
-	high = ans
-	ans = (high + low)/2.0
-	print('numGuesses = ' + str(numGuesses))
-	print(str(ans) + ' is close to square root of ' + str(x))
+	guess = 0
+	epsilon =0.01
+	step =0.1
+	squrt = int(input())
+	while guess < squrt:
+		if abs(guess**2 - squrt) < epsilon:
+			break
+		else:
+			guess = guess + step
+	print(str(guess))
 
 if __name__== "__main__":
 	main()
