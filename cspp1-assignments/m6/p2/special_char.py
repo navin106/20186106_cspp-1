635276efa0ddb54930c3770d8a30945f4bd8677b
navin106 <sandula.navin@msitprogram.net>
2018-08-04 16:41:15 +0530

    " -> 0 of 7 passed. pylint: 0/0 "

--- cspp1-assignments/m6/p2/special_char.py
+++ cspp1-assignments/m6/p2/special_char.py
@@ -7,15 +7,15 @@ def main():
     '''
     Read string from the input, store it in variable str_input.
     '''
-    str_input = input()
+    str_1 = 'abcdef!@'#input()
     str_input1 =''
     temp=0
-    for char in str_input:
+    for char in str_1:
     	if char in '!@#$%^&*':
-    		str_input1[temp] = str_input1[temp] + ' '
+    		str_input1 = str_input1 + ' '
     	else:
-    		str_input1[temp] = str_input1[temp] + str_input[temp]
-    	temp =temp+1
+    		str_input1 = str_input1 + char
+   	temp =temp+1
     print(str_input1)
     
 
