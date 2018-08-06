74db924befe690c42dc4ab27d574eda9c72cda35
navin106 <sandula.navin@msitprogram.net>
2018-08-07 03:23:12 +0530

    " -> 10 of 10 passed. pylint: 6.43/10 "

--- cspp1-assignments/m7/p1/Functions - Assignment-1/assignment1.py
+++ cspp1-assignments/m7/p1/Functions - Assignment-1/assignment1.py
@@ -1,28 +1,22 @@
 '''
 @author: navin106
 # Functions | Assignment-1 - Paying Debt off in a Year
-
 # Write a program to calculate the credit card balance after one year\
  if a person only pays the minimum monthly payment required by the
 # credit card company each month.
-
 # The following variables contain values as described below:
 # balance - the outstanding balance on the credit card
 # annualInterestRate - annual interest rate as a decimal
 # monthlyPaymentRate - minimum monthly payment rate as a decimal
-
 # For each month, calculate statements on the monthly\
  payment and remaining balance. At the end of 12 months, print out the remaining
 # balance. Be sure to print out no more than two decimal digits of accuracy - so print
-
 # Remaining balance: 813.41
 # instead of
 # Remaining balance: 813.4141998135
-
 # So your program only prints out one thing: \
 the remaining balance at the end of the year in the format:
 # Remaining balance: 4784.0
-
 # A summary of the required math is found below:
 # Monthly interest rate= (Annual interest rate) / 12.0
 # Minimum monthly payment = (Minimum monthly payment rate) x (Previous balance)
@@ -30,27 +24,23 @@ the remaining balance at the end of the year in the format:
 # Updated balance each month =\
  (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
 '''
-
 def payingDebtOffInAYear(balance, annualInterestRate, monthlyPaymentRate):
-	'''
-	function to find final balance
-	'''
-	for i in range(12):
-		unpaid_balance = balance - monthlyPaymentRate*balance
-		new_balance = unpaid_balance + (annualInterestRate/12.0)*unpaid_balance
-		i = new_balance
-		balance = i
-	return round(i, 2)
-
+    '''
+    function to find final balance
+    '''
+    for i in range(12):
+        unpaid_balance = balance - monthlyPaymentRate*balance
+        new_balance = unpaid_balance + (annualInterestRate/12.0)*unpaid_balance
+        i = new_balance
+        balance = i
+    return round(i, 2)
 def main():
-	'''
-	function to call paying debt
-	'''
-	data = input()
-	data = data.split(' ')
-	data = list(map(float, data))
-	print('Remaining balance:',payingDebtOffInAYear(data[0],data[1],data[2]))
-
+    '''
+    function to call paying debt
+    '''
+    data = input()
+    data = data.split(' ')
+    data = list(map(float, data))
+    print('Remaining balance:',payingDebtOffInAYear(data[0], data[1], data[2]))
 if __name__ == "__main__":
-	main()
-
+    main()
