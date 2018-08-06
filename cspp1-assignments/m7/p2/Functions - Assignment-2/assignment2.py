7b9f6bd87da970f157d39a13ddda6fb5dc0046fd
navin106 <sandula.navin@msitprogram.net>
2018-08-07 05:19:53 +0530

    " -> 10 of 10 passed. pylint: 10.00/10 "

--- cspp1-assignments/m7/p2/Functions - Assignment-2/assignment2.py
+++ cspp1-assignments/m7/p2/Functions - Assignment-2/assignment2.py
@@ -48,6 +48,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print('Lowest Payment:', paying_debtoffinayear(data[0],data[1]))
+    print('Lowest Payment:', paying_debtoffinayear(data[0], data[1]))
 if __name__ == "__main__":
     main()
