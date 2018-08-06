e0ad98e39967e4f30e3659d65aa258af8703225b
navin106 <sandula.navin@msitprogram.net>
2018-08-06 22:50:44 +0530

    suare of given number

---
+++ cspp1-practice/m7/Functions - Square Exercise/square.py
@@ -0,0 +1,25 @@
+'''
+@author:navin106
+# Exercise: square
+# Write a Python function, square, that takes in one number and returns the square of that number.
+# This function takes in one number and returns one number.
+'''
+def square(x_quare):
+    '''
+    x: int or float.
+    '''
+    return x_quare*x_quare
+# Correct
+def main():
+    '''
+    function to call square
+    '''
+    data = input()
+    data = float(data)
+    temp = str(data).split('.')
+    if temp[1] == '0':
+        print(square(int(float(str(data)))))
+    else:
+        print(square(data))
+if __name__ == "__main__":
+    main()
