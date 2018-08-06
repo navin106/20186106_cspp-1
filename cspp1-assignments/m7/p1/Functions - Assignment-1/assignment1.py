eefa41727ab7c1a444d6c045db1c526db0e7ea22
navin106 <sandula.navin@msitprogram.net>
2018-08-07 03:28:35 +0530

    " -> 10 of 10 passed. pylint: 8.67/10 "

--- cspp1-assignments/m7/p1/Functions - Assignment-1/assignment1.py
+++ cspp1-assignments/m7/p1/Functions - Assignment-1/assignment1.py
@@ -24,14 +24,14 @@ the remaining balance at the end of the year in the format:
 # Updated balance each month =\
  (Monthly unpaid balance) + (Monthly interest rate x Monthly unpaid balance)
 '''
-def paying_DebtOffInAYear(previous_balance, annual_interestRate, monthly_paymentRate):
+def paying_debtOffinaYear(previous_balance, annual_interestrate, monthly_paymentRate):
     '''
     function to find final balance
     '''
     i = 0
     for i in range(12):
         unpaid_balance = previous_balance - monthly_paymentRate*previous_balance
-        new_balance = unpaid_balance + (annual_interestRate/12.0)*unpaid_balance
+        new_balance = unpaid_balance + (annual_interestrate/12.0)*unpaid_balance
         i = new_balance
         previous_balance = i
     return round(i, 2)
@@ -42,6 +42,6 @@ def main():
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print('Remaining balance:', paying_DebtOffInAYear(data[0], data[1], data[2]))
+    print('Remaining balance:', paying_debtOffinaYear(data[0], data[1], data[2]))
 if __name__ == "__main__":
     main()
