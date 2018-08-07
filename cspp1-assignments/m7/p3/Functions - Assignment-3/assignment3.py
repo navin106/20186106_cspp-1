d9faf74561062e82d4f3c43ef7aca1647b25993d
navin106 <sandula.navin@msitprogram.net>
2018-08-08 03:30:05 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- cspp1-assignments/m7/p3/Functions - Assignment-3/assignment3.py
+++ cspp1-assignments/m7/p3/Functions - Assignment-3/assignment3.py
@@ -1,70 +1,40 @@
-# Functions - Assignment-3 - Using Bisection Search to Make the Program Faster
-
-# You'll notice that in Problem 2, your monthly payment had to be a multiple of $10. Why did we make it that way? You can try running
-# your code locally so that the payment can be any dollar and cent amount (in other words, the monthly payment is a multiple of $0.01).
-# Does your code still work? It should, but you may notice that your code runs more slowly, especially in cases with very large balances
-# and interest rates. (Note: when your code is running on our servers, there are limits on the amount of computing time each submission
-# is allowed, so your observations from running this experiment on the grading system might be limited to an error message complaining
-# about too much time taken.)
-
-# Well then, how can we calculate a more accurate fixed monthly payment than we did in Problem 2 without running into the problem of slow
-# code?
-# We can make this program run faster using a technique introduced in lecture - bisection search!
-
-# The following variables contain values as described below:
-# balance - the outstanding balance on the credit card
-# annualInterestRate - annual interest rate as a decimal
-
-# To recap the problem: we are searching for the smallest monthly payment such that we can pay off the entire balance within a year. What
-# is a reasonable lower bound for this payment value? $0 is the obvious anwer, but you can do better than that. If there was no interest,
-# the debt can be paid off by monthly payments of one-twelfth of the original balance, so we must pay at least this much every month.
-# One-twelfth of the original balance is a good lower bound.
-
-# What is a good upper bound? Imagine that instead of paying monthly, we paid off the entire balance at the end of the year. What we
-# ultimately pay must be greater than what we would've paid in monthly installments, because the interest was compounded on the balance
-# we didn't pay off each month. So a good upper bound for the monthly payment would be one-twelfth of the balance, after having its
-# interest compounded monthly for an entire year.
-
-# In short:
-# Monthly interest rate = (Annual interest rate) / 12.0
-# Monthly payment lower bound = Balance / 12
-# Monthly payment upper bound = (Balance x (1 + Monthly interest rate)12) / 12.0
-
-# Write a program that uses these bounds and bisection search (for more info check out the Wikipedia page on bisection search) to find
-# the smallest monthly payment to the cent (no more multiples of $10) such that we can pay off the debt within a year. Try it out with
-# large inputs, and notice how fast it is (try the same large inputs in your solution to Problem 2 to compare!). Produce the same return
-# value as you did in Assignment 2.
-
-
-
+'''
+@author:navin106
+'''
 def payingDebtOffInAYear(balance, annualInterestRate):
-    m_i_r = (annualInterestRate)/12.0
-    monthly_low = balance/12
-    monthly_high = (balance*(1+m_i_r)**12)/12.0
-    new_bal =balance
-    epsilon =0.0001
-    guess = (monthly_low+monthly_high)/2
-    while True:
-        month = 1
-        while month <=12:
-            new_bal = new_bal-guess
-            new_bal = new_bal + (m_i_r*new_bal)
-            month = month+1
-        if new_bal>0 and new_bal>epsilon:
-            monthly_low = guess
-            new_bal =balance
-        elif new_bal < 0 and new_bal <-epsilon:
-            monthly_high =guess
-            new_bal = balance
-        else:
-            return guess
-    guess = (monthly_low + monthly_high)/2    
+	'''
+	function to find fixed amount in bisection method
+	'''
+	mon_intrest = (annualInterestRate)/12.0
+	mon_low_val = balance/12
+	mon_high_val = (balance*(1 +mon_intrest)**12)/12.0
+	new_bal = balance
+	epsilon =0.0001
+	guess = (mon_low_val+mon_high_val)/2
+	while True:
+		month = 1
+		while (month<=12):
+			new_bal = new_bal - guess
+			new_bal = new_bal + (mon_intrest*new_bal)
+			month = month + 1
+		if new_bal > 0 and new_bal > epsilon:
+			mon_low_val = guess
+			new_bal = balance
+		elif new_bal < 0 and new_bal < -epsilon:
+			mon_high_val = guess
+			new_bal = balance
+		else:
+			return guess
+		guess = (mon_low_val + mon_high_val)/2
 def main():
-    # data = "4773 0.2"
+	'''
+	calling function
+	# data = "4773 0.2"
+	'''
     data = input()
     data = data.split(' ')
     data = list(map(float, data))
-    print('Lowest Payment:'+ str(round(payingDebtOffInAYear(data[0],data[1])))
+    print(payingDebtOffInAYear(data[0],data[1]))
     
 if __name__== "__main__":
     main()
