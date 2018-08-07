fb6fed128fc672e89d4d548d2ab18bd0b907326c
navin106 <sandula.navin@msitprogram.net>
2018-08-08 00:12:57 +0530

    gcdrecursive

--- cspp1-practice/m8/GCD using Recursion/gcd_recr.py
+++ cspp1-practice/m8/GCD using Recursion/gcd_recr.py
@@ -1,21 +1,31 @@
 '''
+@author: navin106
 # Exercise: GCDRecr
-# Write a Python function, gcdRecur(a, b), that takes in two numbers and returns the GCD(a,b) of given a and b.
+# Write a Python function, gcd_recur(a, b), that takes in two\
+ numbers and returns the GCD(a,b) of given a and b.
 # This function takes in two numbers and returns one number.
 '''
-def gcdRecur(a, b):
+def gcd_recur(a_int, b_int):
     '''
-    a, b: positive integers 
-    returns: a positive integer, the greatest common divisor of a & b.
+    a_int, b_int: positive integers 
+    returns: a_int positive integer, the greatest common divisor of a_int & b_int.
     '''
-    if b == 0:
-    	return 1
-    return gcdRecur(b, a%b)
-
+    if a_int == 0 or b_int == 0:
+        return 0
+    elif a_int%b_int == 0:
+        return b_int
+    elif b_int%a_int == 0:
+        return a_int
+    elif a_int > b_int:
+        return gcd_recur(b_int, a_int%b_int)
+    else:
+        return gcd_recur(a_int, b_int%a_int)
 def main():
-    data = '2 12'#input()
+    '''
+    calling function
+    '''
+    data = input()
     data = data.split()
-    print(gcdRecur(int(data[0]),int(data[1])))
-
+    print(gcd_recur(int(data[0]), int(data[1])))
 if __name__ == "__main__":
     main()
