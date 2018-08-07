2d2dac7f252aa23407112e9dca6a3f03e0132796
navin106 <sandula.navin@msitprogram.net>
2018-08-07 19:42:59 +0530

    " -> 10 of 10 passed. pylint: 9.09/10 "

--- cspp1-assignments/m8/p1/assignment1.py
+++ cspp1-assignments/m8/p1/assignment1.py
@@ -1,14 +1,14 @@
 '''
 @author:navin106
 # Exercise: Assignment-1
-# Write a Python function, factorial(fac_number), that takes \
+# Write a_input Python function, factorial(fac_number), that takes \
 in one number and returns the factorial of given number.
 # This function takes in one number and returns one number.
 '''
 def factorial(fac_number):
     '''
     fac_number is positive Integer
-    returns: a positive integer, the factorial of fac_number.
+    returns: a_input positive integer, the factorial of fac_number.
     '''
     if fac_number == 0:
         return 1
@@ -20,7 +20,7 @@ def main():
     '''
     caaling factorial function
     '''
-    a = input()
-    print(factorial(int(a)))
+    a_input = input()
+    print(factorial(int(a_input)))
 if __name__ == "__main__":
     main()
