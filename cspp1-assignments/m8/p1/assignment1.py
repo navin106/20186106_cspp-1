6148a9a524a99e36ceb830e4171d668a4ec471bd
navin106 <sandula.navin@msitprogram.net>
2018-08-07 19:37:33 +0530

    " -> 10 of 10 passed. pylint: -0.91/10 "

--- cspp1-assignments/m8/p1/assignment1.py
+++ cspp1-assignments/m8/p1/assignment1.py
@@ -10,13 +10,12 @@ def factorial(n):
 
     returns: a positive integer, the factorial of n.
     '''
-    if n == 1:
+    if n==0:
+    	return 1
+    elif n == 1:
     	return 1
     else:
     	return n*factorial(n-1)
-    
-
-
 def main():
     a = input()
     print(factorial(int(a)))    
