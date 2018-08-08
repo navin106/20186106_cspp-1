364a260b73538e8ae15eb00d091497eb0859374e
navin106 <sandula.navin@msitprogram.net>
2018-08-08 23:14:58 +0530

    " -> 4 of 9 passed. pylint: 8.42/10 "

--- cspp1-assignments/m9/p1/assignment1.py
+++ cspp1-assignments/m9/p1/assignment1.py
@@ -14,6 +14,7 @@ def is_word_guessed(secret_word, letters_guessed):
     returns: boolean, True if all the letters of secret_word are in letters_guessed;
       False otherwise
     '''
+    count = 0
     for i in secret_word:
     	if i in letters_guessed:
     		count = count + 1
