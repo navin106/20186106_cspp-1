347cb1f39d9aca399105f6d2c818a6c9c956c9d8
navin106 <sandula.navin@msitprogram.net>
2018-08-11 16:53:28 +0530

    " -> 0 of 3 passed. pylint: 0/0 "

--- cspp1-assignments/m12/p1/create_social_network.py
+++ cspp1-assignments/m12/p1/create_social_network.py
@@ -30,15 +30,7 @@ def create_social_network(data):
         Error handling case:
         Return a empty dictionary if the string format of the data is invalid
         Empty dictionary is not None, it is a dictionary with no keys
-    '''
-    count = 0
-    count1 = 0
-    for i in data:
-        if i == ' follows ':
-            count = count + 1
-        if i == ',':
-            count1 = count1 + 1
-    if count > 0 and count1 > 0:        
+    '''        
         l_enter = []
         k_follows = []
         m_final = []
@@ -50,11 +42,11 @@ def create_social_network(data):
         for i in range(len(l_revised)):
             k_follows = l_revised[i].split(' follows ')
             m_final.extend(k_follows)
-        for i in range(0,len(m_final),2):
-            if m_final[i] not in s_dict:
-                s_dict[m_final[i]] = m_final[i+1].split(',')
-        return s_dict
-    else:
+        if len(m_final) > len(k_follows):
+            for i in range(0,len(m_final),2):
+                if m_final[i] not in s_dict:
+                    s_dict[m_final[i]] = m_final[i+1].split(',')
+            return s_dict
         return {}
 def main():
     '''
