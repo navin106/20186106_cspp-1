0a983c79f1b3da265827bf84630c7fe410338cd6
navin106 <sandula.navin@msitprogram.net>
2018-08-11 18:32:50 +0530

    " -> 0 of 2 passed. pylint: 8.57/10 "

--- cspp1-assignments/m12/p2/social_network.py
+++ cspp1-assignments/m12/p2/social_network.py
@@ -44,7 +44,11 @@ def delete_person(network, arg1):
     also, before deleting arg1, remove arg1 from the everyone's followers list
     update the network dictionary and return it
     '''
-    del  network[arg1]
+
+    del network[arg1]
+    for i in network:
+        if arg1 in network[i]:
+            network[i].remove(arg1)
     new_network = network
     return new_network
 
