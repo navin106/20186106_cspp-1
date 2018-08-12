e5858ebd281f9d7e77a43c2f3ae3e005a4288244
navin106 <sandula.navin@msitprogram.net>
2018-08-12 22:01:05 +0530

    " -> 6 of 6 passed. pylint: 8.75/10 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -1,6 +1,7 @@
 '''
 #Exercise: Assignment-2
-#Implement the update_hand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here,\
+#Implement the update_hand function. Make sure this function has no side effects:\
+ i.e., it must not mutate the hand passed in. Before pasting your function definition here,\
 be sure you've passed the appropriate tests in test_ps4a.py.
 '''
 def update_hand(hand, word):
@@ -8,7 +9,7 @@ def update_hand(hand, word):
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
     Has no side effects: does not modify hand.
