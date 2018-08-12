0a1ccbca17508be0a310784547e39a1d7622e98f
navin106 <sandula.navin@msitprogram.net>
2018-08-13 00:22:43 +0530

    " -> 5 of 5 passed. pylint: 2.67/10 "

--- cspp1-assignments/m11/p4/assignment4.py
+++ cspp1-assignments/m11/p4/assignment4.py
@@ -1,33 +1,32 @@
 #Exercise: Assignment-4
 '''
+@author:navin106
 #We are now ready to begin writing the code that interacts with the player. \
 We'll be implementing the playHand function. \
 This function allows the user to play out a single hand. \
 First, though, you'll need to implement the helper calculateHandlen function, \
 which can be done in under five lines of code.
 '''
-
 def calculateHandlen(hand):
     """ 
     Returns the length (number of letters) in the current hand.
-    
     hand: dictionary (string int)
     returns: integer
     """
     k = 0
     for i in hand:
-    	k = k + hand[i]
+        k = k + hand[i]
     return k 
 def main():
-	n=input()
-	adict={}
-	for i in range(int(n)):
-		data=input()
-		l=data.split()
-		adict[l[0]]=int(l[1])
-	print(calculateHandlen(adict))
-		
-
-
-if __name__== "__main__":
-	main()
+    '''
+    calling function
+    '''
+    n=input()
+    adict={}
+    for data in range(int(n)):
+        data=input()
+        l=data.split()
+        adict[l[0]]=int(l[1])
+    print(calculateHandlen(adict))
+if __name__ == "__main__":
+    main()
