23614ab94dd23e054e9bf88c677075e5e536b77c
navin106 <sandula.navin@msitprogram.net>
2018-08-16 20:00:26 +0530

    " -> 6 of 19 passed. pylint: 8.67/10 "

--- cspp1-assignments/m15/CodeCampPoker/poker.py
+++ cspp1-assignments/m15/CodeCampPoker/poker.py
@@ -74,13 +74,20 @@ def is_two(hand):
     '''
     face_values3 = []
     count2 = 0
+    a = ()
+    k = 0
     for i in hand:
         face_values3.append(VAL_DICT[i[0]])
     face_values3.sort()
     for k in range(len(face_values3)-1):
         if face_values3[k+1]-face_values3[k] == 0:
             count2 += 1
-    return count2 == 1
+    if count2 == 1:
+    	while True:
+    		a[k] = hand
+    		i += 1
+    return max(a) == a[k]
+
 def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
