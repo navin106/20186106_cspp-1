07e530edbd9edbcfe1d3718ea78cc5f9d7af2cce
navin106 <sandula.navin@msitprogram.net>
2018-08-16 17:58:43 +0530

    " -> 0 of 19 passed. pylint: 7.70/10 "

--- cspp1-assignments/m15/CodeCampPoker/poker.py
+++ cspp1-assignments/m15/CodeCampPoker/poker.py
@@ -6,6 +6,16 @@
 '''
 VAL_DICT = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8,\
      '9':9, 'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+def face_values(hand):
+    face_values = []
+    for i in hand:
+        face_vales.append(VAL_DICT[i[0]])
+    return face_values
+def suit_values(hand):
+    suit_values = []
+    for i in hand:
+        suit_values.append(i[1])
+    return suit_values
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
@@ -16,17 +26,15 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
-    face_values = []
-    for i in hand:
-        face_values.append(VAL_DICT[i[0]])
-    face_values.sort()
-    for k in range(len(face_values)-1):
-        if face_values[k+1]-face_values[k] != 1:
+    new_l = face_values(hand).sort()
+    for k in range(len(new_l)-1):
+        if new_l[k+1]-new_l[k] != 1:
             return False
     return True
 def is_four(hand):
     '''
     checks weather it is a four of a kind or not and sends the true or false
+    '''
     '''
     face_values1 = []
     count = 0
@@ -37,6 +45,7 @@ def is_four(hand):
         if face_values1[k+1]-face_values1[k] == 0:
             count += 1
     return count == 3
+    '''
 
 def is_flush(hand):
     '''
@@ -47,6 +56,7 @@ def is_flush(hand):
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
     '''
+    '''
     suit_list = []
     sum_ascii = 0
     for i in hand:
@@ -56,6 +66,12 @@ def is_flush(hand):
     if sum_ascii == 5*ord(i):
         return True
     return False
+    '''
+    k = hand[i]
+    for i in suit_values(hand):
+        if k != i:
+            return False
+    return True
 def is_three(hand):
     '''
     check weather the given hand is three of a kind
@@ -85,16 +101,6 @@ def is_two(hand):
                 temp = face_values3[k]
                 count2 += 1
     return count2 == 1
-    '''
-def high_card(hand):
-    face_valuesh = []
-    count1 = 0
-    for i in hand:
-        face_valuesh.append(VAL_DICT[i[0]])
-    face_valuesh.sort()
-    temp = sum(hand)
-    if temp <
-    '''
 def hand_rank(hand):
     '''
         You will code this function. The goal of the function is to
@@ -113,12 +119,10 @@ def hand_rank(hand):
     # Instead break it down into two sub functions is_straight and is_flus
     # check for straight, flush and straight flush
     if is_straight(hand) and is_flush(hand):
-        retur = 7
-    elif is_four(hand):
         retur = 6
-    # best hand of these 3 would be a straight flush with the return value 3
-    elif is_three(hand) and is_two(hand):
+    elif is_four(hand):
         retur = 5
+    # best hand of these 3 would be a straight flush with the return value 3
     elif is_flush(hand):
         retur = 4
     # the second best would be a flush with the return value 2
@@ -129,8 +133,7 @@ def hand_rank(hand):
     elif is_two(hand):
         retur = 1
     else:
-        if high_card(hand):
-            retur = 0
+        retur = 0
     return retur
     # third would be a straight with the return value 1
     # any other hand would be the fourth best with the return value 0
