31f7d47f8239990061d1fa981722cc958edc42e4
navin106 <sandula.navin@msitprogram.net>
2018-08-17 18:06:02 +0530

    " -> 1 of 3 passed. pylint: 3.48/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -31,13 +31,11 @@ def similarity(dict1, dict2):
     			new_dict2[i] = 1
     		else:
     			new_dict2[i] += 1
-    
     for i in new_dict1:
     	if i not in new_dict2:
-    		big_dict[i] = [0,new_dict2[i]]
+    		big_dict[i] = [new_dict1[i],0]
     	else:
     		big_dict[i] = [new_dict1[i], new_dict2[i]]
-    
     for i in big_dict:
     	c = c + big_dict[i][0]*big_dict[i][1]
     for i in big_dict:
