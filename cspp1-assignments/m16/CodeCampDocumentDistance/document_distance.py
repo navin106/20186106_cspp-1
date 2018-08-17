7971ab796a5c3d015a4e6da9667de52a119492e8
navin106 <sandula.navin@msitprogram.net>
2018-08-17 22:12:06 +0530

    " -> 3 of 3 passed. pylint: 7.40/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -1,7 +1,7 @@
 '''
 @author:navin106
 activity 1 module 16
-    Document Distance - A detailed description is given in the PDF
+Document Distance - A detailed description is given in the PDF
 '''
 import re
 import math
@@ -16,44 +16,38 @@ def similarity(dict1, dict2):
     d = 0
     e = 0
     res = 0
-    new_list1 = (((re.sub(r'[^\w\s]','',dict1)).lower()).replace('\'','')).split()
-    new_list2 = (((re.sub(r'[^\w\s]','',dict2)).lower()).replace('\'','')).split()
+    new_list1 = ((re.sub(r'[^\w\s]','',dict1)).lower()).split()
+    new_list2 = ((re.sub(r'[^\w\s]','',dict2)).lower()).split()
     stop_words = load_stopwords("stopwords.txt")
     for i in new_list1:
-    	if i not in stop_words and i not in '1234567890':
-    		if i not in new_dict1:
-    			new_dict1[i] = 1
-    		else:
-    			new_dict1[i] += 1
-    
+        if i not in stop_words and i not in '1234567890':
+            if i not in new_dict1:
+                new_dict1[i] = 1
+            else:
+                new_dict1[i] += 1
     for i in new_list2:
-    	if i not in stop_words and i not in '1234567890':
-    		if i not in new_dict2:
-    			new_dict2[i] = 1
-    		else:
-    			new_dict2[i] += 1
-    
+        if i not in stop_words and i not in '1234567890':
+            if i not in new_dict2:
+                new_dict2[i] = 1
+            else:
+                new_dict2[i] += 1
     for i in new_dict1:
-
-    	if i not in new_dict2:
-    		big_dict[i] = [new_dict1[i], 0]
-    	else:
-    		big_dict[i] = [new_dict1[i], new_dict2[i]]
+        if i not in new_dict2:
+            big_dict[i] = [new_dict1[i], 0]
+        else:
+            big_dict[i] = [new_dict1[i], new_dict2[i]]
     for j in new_dict2:
-    	if j not in new_dict1:
-    		big_dict[j] = [0, new_dict2[j]]
-    	else:
-    		big_dict[j] = [new_dict1[j], new_dict2[j]]
+        if j not in new_dict1:
+            big_dict[j] = [0, new_dict2[j]]
+        else:
+            big_dict[j] = [new_dict1[j], new_dict2[j]]
     for i in big_dict:
-    	c = c + big_dict[i][0]*big_dict[i][1]
-    
+        c = c + big_dict[i][0]*big_dict[i][1]
     for i in big_dict:
-    	d = d + big_dict[i][0]**2
-    	e = e + big_dict[i][1]**2
-    
+        d = d + big_dict[i][0]**2
+        e = e + big_dict[i][1]**2
     res = c/(math.sqrt(d)*math.sqrt(e)) 
     return res
-
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
@@ -63,7 +57,6 @@ def load_stopwords(filename):
         for line in filename:
             stopwords[line.strip()] = 0
     return stopwords
-
 def main():
     '''
         take two inputs and call the similarity function
