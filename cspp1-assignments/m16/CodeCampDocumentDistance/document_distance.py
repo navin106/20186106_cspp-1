ce7eb467d5cebca7c4bcca11a99f11a9f9692d44
navin106 <sandula.navin@msitprogram.net>
2018-08-18 00:29:02 +0530

    " -> 1 of 3 passed. pylint: 7.97/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -10,6 +10,7 @@ def clean_string(string):
     send only alphabetics words without punctuations
     '''
     string = string.replace('\'','')
+    string =string.strip()
     clean_list = ((re.sub(r'[^\w\s]', '', string)).lower()).split()
     return clean_list
 def vector_dictionary(new_list1, new_list2):
