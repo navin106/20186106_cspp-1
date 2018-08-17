2d378287b12f5059d4a318a197ba94b5cf51a451
navin106 <sandula.navin@msitprogram.net>
2018-08-18 00:25:11 +0530

    " -> 1 of 3 passed. pylint: 8.60/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -34,10 +34,10 @@ def vector_dictionary(new_list1, new_list2):
     for i in new_dict1 and new_dict2:
         big_dict[i] = [new_dict1[i], new_dict2[i]] 
     for i in new_dict1:
-        if i not in new_dict2:
+        if i not in big_dict:
             big_dict[i] = [new_dict1[i], 0]
     for i in new_dict2:
-        if i not in new_dict1:
+        if i not in big_dict:
             big_dict[i] = [0, new_dict2[j]]
     '''
     for i in new_dict1:
