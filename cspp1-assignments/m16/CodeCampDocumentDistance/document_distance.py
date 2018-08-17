8fd81c3d32161566fb7c13b863368cb8924fdbb7
navin106 <sandula.navin@msitprogram.net>
2018-08-17 16:55:34 +0530

    " -> 0 of 3 passed. pylint: 6.67/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -8,14 +8,19 @@ def similarity(dict1, dict2):
     '''
         Compute the document distance as given in the PDF
     '''
-    new_dict1 = []
+    new_udlist1 = []
+    new_udlist2 = []
     new_list1 = ((re.sub(r'[^\w\s]','',dict1)).lower()).split()
     new_list2 = ((re.sub(r'[^\w\s]','',dict2)).lower()).split()
     stop_words = load_stopwords("stopwords.txt")
     for i in new_list1:
     	if i not in stop_words:
-    		new_dict1.append(i)
-    print(new_dict1)
+    		new_udlist1.append(i)
+    for i in new_list2:
+    	if i not in stop_words:
+    		new_udlist2.append(i)
+    print(new_udlist1)
+    print(new_udlist2)
 def load_stopwords(filename):
     '''
         loads stop words from a file and returns a dictionary
