eb27046320d6b865059a49eb55ed6cecfc585edf
navin106 <sandula.navin@msitprogram.net>
2018-08-17 20:12:48 +0530

    " -> 0 of 3 passed. pylint: 2.00/10 "

--- cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -16,7 +16,7 @@ def similarity(dict1, dict2):
     d = 0
     e = 0
     res = 0
-    new_list1 = (((re.sub(r'[^\w\s]',' ',dict1)).lower()).strip()).split(' ')
+    new_list1 = (((re.compile('[^a-z]')).sub('',dict1)).lower()).split('')
     new_list2 = (((re.sub(r'[^\w\s]',' ',dict2)).lower()).strip()).split(' ')
     stop_words = load_stopwords("stopwords.txt")
     for i in new_list1:
