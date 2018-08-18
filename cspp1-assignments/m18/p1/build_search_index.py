ea5e87637413e47d813cd308bb1a1d1e8d8c9997
navin106 <sandula.navin@msitprogram.net>
2018-08-18 15:56:25 +0530

    " -> 0 of 1 passed. pylint: 0/0 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -91,7 +91,7 @@ def main():
         i += 1
 
     # call print to display the search index
-    print_search_index(build_search_index(documents))
+    build_search_index(documents)
 
 if __name__ == '__main__':
     main()
