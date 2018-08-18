ee16398ced4bca79e665e098c75940ae3dea4b7f
navin106 <sandula.navin@msitprogram.net>
2018-08-18 16:57:45 +0530

    " -> 0 of 1 passed. pylint: 9.51/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -64,10 +64,8 @@ def build_search_index(docs):
                     if k not in search_index:
                         search_index[k] = [(temp, 1)]
                     else:
-                        temp2 = search_index[k][0][1]
-                        temp2 += 1
                         search_index[k] = []
-                        search_index[k].append([(temp, temp2)])
+                        search_index[k].append([(temp, 1)])
     temp += 1
         # add or update the words of the doc to the search index
     # return search index
