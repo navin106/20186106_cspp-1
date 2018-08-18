a214d1ff2ac969ce35e571aa0d48156af9f5e3a0
navin106 <sandula.navin@msitprogram.net>
2018-08-18 17:49:09 +0530

    " -> 0 of 1 passed. pylint: 9.30/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -48,7 +48,7 @@ def build_search_index(docs):
     '''
     req_list = []
     for i in range(len(docs)-1):
-    	req_list.append(sorted(word_list(docs[i])))
+        req_list.append(sorted(word_list(docs[i])))
     # initialize a search index (an empty dictionary)
     search_index = {}
     stop_words = load_stopwords("stopwords.txt")
@@ -57,18 +57,18 @@ def build_search_index(docs):
     for i in req_list:
     # keep track of doc_id which is the list index corresponding the document
     # hint: use enumerate to obtain the list index in the for loop
-    	for k in i:
-    		if k not in stop_words: 
+        for k in i:
+            if k not in stop_words: 
         # clean up doc and tokenize to words list
-    			if len(k) > 1:
-    				if k not in search_index:
-    					search_index[k] = [(temp, 1)]
-    				else:
+                if len(k) > 1:
+                    if k not in search_index:
+                        search_index[k] = [(temp, 1)]
+                    else:
                         temp2 = search_index[k][1]
                         temp2 +=1
                         search_index[k] = []
-    					search_index[k].append([(temp, temp2)])
-    	temp += 1
+                        search_index[k].append([(temp, temp2)])
+        temp += 1
         # add or update the words of the doc to the search index
     # return search index
     return search_index
