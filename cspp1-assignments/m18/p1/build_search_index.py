ae1d0be9366a37f79295ad6d03e38c49f7d1ded1
navin106 <sandula.navin@msitprogram.net>
2018-08-18 16:18:42 +0530

    " -> 0 of 1 passed. pylint: 7.27/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -66,7 +66,7 @@ def build_search_index(docs):
     # keep track of doc_id which is the list index corresponding the document
     			else:
     				print(search_index[i][1])
-    				search_index[i] = [temp, search_index[i][1] += 1]
+    				#search_index[i] = [temp, search_index[i][1] += 1]
     	temp += 1
     # hint: use enumerate to obtain the list index in the for loop
     print(search_index)
