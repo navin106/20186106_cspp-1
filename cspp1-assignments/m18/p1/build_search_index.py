2860f94a9b1f4d85ef5aa2509cc044bf3cba0450
navin106 <sandula.navin@msitprogram.net>
2018-08-18 18:07:35 +0530

    " -> 0 of 1 passed. pylint: 8.91/10 "

--- cspp1-assignments/m18/p1/build_search_index.py
+++ cspp1-assignments/m18/p1/build_search_index.py
@@ -50,6 +50,7 @@ def build_search_index(docs):
     for i in range(len(docs)-1):
         req_list.append(sorted(word_list(docs[i])))
     # initialize a search index (an empty dictionary)
+    print(req_list)
     search_index = {}
     stop_words = load_stopwords("stopwords.txt")
     # iterate through all the docs
