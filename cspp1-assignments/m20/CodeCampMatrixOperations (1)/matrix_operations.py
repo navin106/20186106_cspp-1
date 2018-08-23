1dbd1f5b22ed28f4f762b9e409c35a4249983bbd
navin106 <sandula.navin@msitprogram.net>
2018-08-24 03:46:44 +0530

    " -> 8 of 8 passed. pylint: 9.70/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations (1)/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations (1)/matrix_operations.py
@@ -13,10 +13,9 @@ def mult_matrix(mat_1, mat_2):
     if len(mat_1[1]) != len(mat_2):
         print("Error: Matrix shapes invalid for mult")
         return None
-    else:
-        result = [[sum(x * y for x, y in zip(m1_row, m2_col)) for m2_col in zip(*mat_2)] \
-        for m1_row in mat_1]
-        return result
+    result = [[sum(x * y for x, y in zip(m1_row, m2_col)) for m2_col in zip(*mat_2)] \
+    for m1_row in mat_1]
+    return result
 def add_matrix(mat_1, mat_2):
     '''
         check if the matrix shapes are similar
@@ -28,9 +27,8 @@ def add_matrix(mat_1, mat_2):
     if len(mat_1) == len(mat_2) and len(mat_1[0]) == len(mat_2[0]):
         k = [i + j for x, y in zip(mat_1, mat_2) for i, j in zip(x, y)]
         return [k[x:x+len(mat_1[1])] for x in range(0, len(k), len(mat_1[1]))]
-    else:
-        print("Error: Matrix shapes invalid for addition")
-        return None
+    print("Error: Matrix shapes invalid for addition")
+    return None
 def read_matrix():
     '''
         read the matrix dimensions from input
@@ -40,7 +38,7 @@ def read_matrix():
         error message should be "Error: Invalid input for the matrix"
     '''
     matrix = []
-    flag = 0
+    # flag = 0
     inp_list = input().split(',')
     rows = int(inp_list[0])
     columns = int(inp_list[1])
