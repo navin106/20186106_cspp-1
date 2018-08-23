c26b49c1a49d6bedf3374342497a213f90e87db6
navin106 <sandula.navin@msitprogram.net>
2018-08-24 00:58:54 +0530

    " -> 0 of 8 passed. pylint: 0/0 "

--- cspp1-assignments/m20/CodeCampMatrixOperations (1)/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations (1)/matrix_operations.py
@@ -45,18 +45,15 @@ def read_matrix():
             matrix.append([int(i) for i in l])
         else:
             print("Error: Invalid input for the matrix")
-            return None
     return matrix
 
 def main():
     # read matrix 1
     m1 = read_matrix()
     # add matrix 1 and matrix 2
-    if m1 is None:
         exit()
     # read matrix 2
     m2 = read_matrix()
-    if m2 is None:
         exit()
     print(add_matrix(m1, m2))
     print(mult_matrix(m1, m2))
