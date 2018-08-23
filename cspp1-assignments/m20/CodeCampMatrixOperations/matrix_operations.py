d151fdf13c5ce2ff40303173fd0735390e48aebf
navin106 <sandula.navin@msitprogram.net>
2018-08-23 22:37:04 +0530

    " -> 0 of 8 passed. pylint: 3.12/10 "

--- cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
+++ cspp1-assignments/m20/CodeCampMatrixOperations/matrix_operations.py
@@ -41,11 +41,11 @@ def read_matrix(n):
     matrix = []
     for row in range(int(n[0])):
         l = input().split(' ')
-        if (len(l) != len(n[0])):
+        if (len(l) == len(n[0])):
+            matrix.append([int(l[i]) for i in range(len(l))])
+        else:
             print("Error: Invalid input for the matrix")
             return None
-        else:
-            matrix.append([int(l[i]) for i in range(len(l))])
     return matrix
 
 def main():
