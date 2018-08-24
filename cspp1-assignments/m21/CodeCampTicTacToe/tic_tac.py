a3952f213c51992d89d9b85d8e50b2b633ae0084
navin106 <sandula.navin@msitprogram.net>
2018-08-24 15:52:28 +0530

    " -> 0 of 14 passed. pylint: -6.67/10 "

---
+++ cspp1-assignments/m21/CodeCampTicTacToe/tic_tac.py
@@ -0,0 +1,30 @@
+def iscount(matrix, chk_chr):
+	'''
+	returns sum of count of the char
+	'''
+	k = []
+	for i in matrix:
+		k.append(i.count(chk_chr))
+	return sum(k)
+def isinvalid(matrix):
+	'''
+	return bool for input is valid or not
+	'''
+	for i in matrix:
+		for j in i:
+			if j not in 'xo.':
+				return 'invalid input'
+	if iscount(matrix,'x') or iscount(matrix,'o') or iscount(matrix,'.') > 5:
+		return 'invalid game'
+
+
+
+def main():
+	'''
+	calling function
+	'''
+	a = []
+	for i in range(3):
+		a.append(input().split())
+	print(isinvalid(matrix))
+main()
