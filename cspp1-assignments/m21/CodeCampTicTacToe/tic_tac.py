c522ec835221b4d5be9cf43c1f1fd461ca9d39ff
navin106 <sandula.navin@msitprogram.net>
2018-08-24 20:05:09 +0530

    " -> 14 of 14 passed. pylint: 9.79/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tic_tac.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tic_tac.py
@@ -49,8 +49,7 @@ def isinvalid(matrix):
     '''
     return bool for input is valid or not
     '''
-    print(set(['f' for i in matrix for j in i if j not in 'xo.']))
-    if ['f' for i in matrix for j in i if j not in 'xo.'] == ['f']:
+    if set(['f' for i in matrix for j in i if j not in 'xo.']) == {'f'}:
         print('invalid input')
         return False
     if iscount(matrix, 'x') > 5 or iscount(matrix, 'o') > 5 or \
