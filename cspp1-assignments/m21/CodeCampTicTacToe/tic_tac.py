7dbe9383ac1bcebea220ebf6530628e88c95a772
navin106 <sandula.navin@msitprogram.net>
2018-08-24 22:02:39 +0530

    " -> 14 of 14 passed. pylint: 10.00/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tic_tac.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tic_tac.py
@@ -45,19 +45,9 @@ def iscount(matrix, chk_chr):
     for i in matrix:
         k.append(i.count(chk_chr))
     return sum(k)
-def is_verified(matrix):
-    '''
-    Checking if the matrix contains 'x' or 'o' or '.'
-    '''
-    for i in matrix:
-        for j in i:
-            if j not in 'xo.':
-                return False
-    return True
 def isinvalid(matrix):
     '''
     return bool for input is valid or not
-    if not is_verified(matrix):
     '''
     if 'f' in ['f' for i in matrix for j in i if j not in 'xo.']:
         print('invalid input')
