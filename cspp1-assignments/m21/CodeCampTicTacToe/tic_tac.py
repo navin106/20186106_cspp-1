2aafe353a7f8e8eec659398e2a9266642babb598
navin106 <sandula.navin@msitprogram.net>
2018-08-24 17:29:52 +0530

    " -> 14 of 14 passed. pylint: 8.00/10 "

--- cspp1-assignments/m21/CodeCampTicTacToe/tic_tac.py
+++ cspp1-assignments/m21/CodeCampTicTacToe/tic_tac.py
@@ -1,81 +1,82 @@
 def iswinnerdiagonal(matrix):
-	'''
-	check diagonal
-	'''
-	r_d = []
-	l_d = []
-	for i in range(len(matrix)):
-		r_d.append(matrix[i][i])
-		l_d.append(matrix[i][len(matrix)-i-1])
-	if r_d.count('x') == 3:
-		print('x')
-		return False
-	elif r_d.count('o') == 3:
-		print('o')
-		return False
-	elif l_d.count('x') == 3:
-		print('x')
-		return False
-	else:
-		if l_d.count('x') == 3:
-			print('x')
-			return False	
-	return True
+    '''
+    check diagonal
+    '''
+    r_d = []
+    l_d = []
+    for i in range(len(matrix)):
+        r_d.append(matrix[i][i])
+        l_d.append(matrix[i][len(matrix)-i-1])
+    if r_d.count('x') == 3:
+        print('x')
+        return False
+    elif r_d.count('o') == 3:
+        print('o')
+        return False
+    elif l_d.count('x') == 3:
+        print('x')
+        return False
+    else:
+        if l_d.count('x') == 3:
+            print('x')
+            return False    
+    return True
 def iswinnerhorizontal(matrix):
-	'''
-	check horizontal
-	'''
-	for i in matrix:
-		if i.count('x') == 3:
-			print('x')
-			return False
-		else:
-			if i.count('o') == 3:
-				print('o')
-				return False
-	return iswinnervertical(matrix)
+    '''
+    check horizontal
+    '''
+    for i in matrix:
+        if i.count('x') == 3:
+            print('x')
+            return False
+        else:
+            if i.count('o') == 3:
+                print('o')
+                return False
+    return iswinnervertical(matrix)
 def iswinnervertical(matrix):
-	'''
-	check vertical
-	'''
-	for i in zip(*matrix):
-		if list(i).count('x') == 3:
-			print('x')
-			return False
-		else:
-			if list(i).count('o') == 3:
-				print('o')
-				return False
-	return iswinnerdiagonal(matrix)
+    '''
+    check vertical
+    '''
+    for i in zip(*matrix):
+        if list(i).count('x') == 3:
+            print('x')
+            return False
+        else:
+            if list(i).count('o') == 3:
+                print('o')
+                return False
+    return iswinnerdiagonal(matrix)
 def iscount(matrix, chk_chr):
-	'''
-	returns sum of count of the char
-	'''
-	k = []
-	for i in matrix:
-		k.append(i.count(chk_chr))
-	return sum(k)
+    '''
+    returns sum of count of the char
+    '''
+    k = []
+    for i in matrix:
+        k.append(i.count(chk_chr))
+    return sum(k)
 def isinvalid(matrix):
-	'''
-	return bool for input is valid or not
-	'''
-	for i in matrix:
-		for j in i:
-			if j not in 'xo.':
-				print('invalid input')
-				return False
-	if iscount(matrix,'x') > 5 or iscount(matrix,'o') > 5 or (iscount(matrix,'x') == 3 and iscount(matrix,'o') == 3):
-		print('invalid game')
-		return False
-	return True
+    '''
+    return bool for input is valid or not
+    '''
+    for i in matrix:
+        for j in i:
+            if j not in 'xo.':
+                print('invalid input')
+                return False
+    if iscount(matrix,'x') > 5 or iscount(matrix,'o') > 5 or \
+    (iscount(matrix,'x') == 3 and iscount(matrix,'o') == 3):
+        print('invalid game')
+        return False
+    return True
 def main():
-	'''
-	calling function
-	'''
-	matrix = []
-	for i in range(3):
-		matrix.append(input().split())
-	if isinvalid(matrix):
-		if iswinnerhorizontal(matrix):
-			print('draw')
+    '''
+    calling function
+    '''
+    matrix = []
+    for i in range(3):
+        matrix.append(input().split())
+    if isinvalid(matrix):
+        if iswinnerhorizontal(matrix):
+            print('draw')
 main()
