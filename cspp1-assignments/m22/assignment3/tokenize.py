91c6f0bcad9d0581976424a04e6b1daeb7f3efbe
navin106 <sandula.navin@msitprogram.net>
2018-08-25 16:34:12 +0530

    " -> 0 of 4 passed. pylint: 9.41/10 "

--- cspp1-assignments/m22/assignment3/tokenize.py
+++ cspp1-assignments/m22/assignment3/tokenize.py
@@ -2,6 +2,7 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
+import re
 def clean_string(string):
     '''
     removing special characters for string
