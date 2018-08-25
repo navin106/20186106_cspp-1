53402f934a419110da083aab9606cbb829b285af
navin106 <sandula.navin@msitprogram.net>
2018-08-25 16:08:15 +0530

    " -> 2 of 2 passed. pylint: 10.00/10 "

--- cspp1-assignments/m22/assignment1/read_input.py
+++ cspp1-assignments/m22/assignment1/read_input.py
@@ -9,7 +9,7 @@ def main():
     no_of_lines = int(input())
     for _ in range(no_of_lines):
         temp_list.append(input())
-    for i in range(no_of_lines-1):
+    for i in range(no_of_lines):
         print(temp_list[i])
 
 if __name__ == '__main__':
