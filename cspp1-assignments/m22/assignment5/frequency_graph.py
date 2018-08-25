95c9ae2be9094df7b03f2279eeecfcaef2c05916
navin106 <sandula.navin@msitprogram.net>
2018-08-25 17:10:57 +0530

    " -> 4 of 4 passed. pylint: 9.00/10 "

--- cspp1-assignments/m22/assignment5/frequency_graph.py
+++ cspp1-assignments/m22/assignment5/frequency_graph.py
@@ -4,18 +4,18 @@ frequency of each word. Display the frequency values using “#” as a text bas
 '''
 
 def frequency_graph(dictionary):
-	'''
-	freq graph of the dictionary
-	'''
+    '''
+    freq graph of the dictionary
+    '''
     sort_keys = sorted(dictionary)
     for i in sort_keys:
         print(i, '-', '#'*dictionary[i])
     return '\n'
 
 def main():
-	'''
-	main function to read and send dictionary
-	'''
+    '''
+    main function to read and send dictionary
+    '''
     dictionary = eval(input())
     frequency_graph(dictionary)
 
